cae3f2e66846c504f90c436d6c6caf1db40bd48f
Enad-Alharbi <enad_alkhtabi@hotmail.com>
2026-07-21 16:45:01 +0300

    Completed Problem #25 (Optimal Solution)

--- problem#25.cpp
+++ problem#25.cpp
@@ -4,34 +4,42 @@ using namespace std;
 
 // This program will ask the user to enter his age, and then validate if his/her age is between 18 and 45, and keep asking the user until he/she enters a valid age.
 
-bool ValidateNumberInRange(int num, int from, int to)
-{
-    return (num >= from && num <= to);
-}
 
 int ReadAge()
 {
     int age;
 
-    cout << "Please enter your age: \n";
+    cout << "Please enter Age between 18 and 45: \n";
     cin >> age;
-    while(!ValidateNumberInRange(age, 18, 45))
+
+    return age;
+}
+
+bool ValidateNumberInRange(int num, int from, int to)
+{
+    return (num >= from && num <= to);
+}
+
+int ReadUntilAgeBetween()
+{
+    int age = 0;
+
+    do
     {
-        cout << "Invalid age, Please enter a valid age (between 18 and 45): \n";
-        cin >> age;
-    }
+       age = ReadAge(); 
+    } while (!ValidateNumberInRange(age, 18, 45));
 
     return age;
 }
 
 void PrintResult(int age)
 {
-    cout << age << " is a valid age!\n";
+    cout << "Your age is: " << age << endl;
 }
 
 int main()
 {
-    PrintResult(ReadAge());
+    PrintResult(ReadUntilAgeBetween());
 
     return 0;
 }
