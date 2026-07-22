2d9a4f2797bf93c2c30adbd5f2f07974fc8210c4
Enad-Alharbi <enad_alkhtabi@hotmail.com>
2026-07-22 17:22:35 +0300

    Completed Probelm #33 (Optimal Solution)

--- problem#33.cpp
+++ problem#33.cpp
@@ -4,27 +4,29 @@ using namespace std;
 
 // This program will ask the user to enter his mark grade, and then will print the grade letter (A, B, C, D, E, F).
 
-int ReadGrade()
+int ReadNumberInRagne(int from, int to)
 {
     int grade;
-
-    cout << "Please enter your grade: \n";
-    cin >> grade;
+    do
+    {
+        cout << "Please enter your grade (0 - 100): \n";
+        cin >> grade;
+    } while (grade < from || grade > to);
 
     return grade;
 }
 
 char PrintGradeLetter(int grade)
 {
-    if (grade >= 90 && grade <= 100)
+    if (grade >= 90)
         return 'A';
-    else if(grade >= 80 && grade <= 89)
+    else if(grade >= 80)
         return 'B';
-    else if(grade >= 70 && grade <= 79)
+    else if(grade >= 70)
         return 'C';
-    else if(grade >= 60 && grade <= 69)
+    else if(grade >= 60)
         return 'D';
-    else if(grade >= 50 && grade <= 59)
+    else if(grade >= 50)
         return 'E';
     else
         return 'F';
@@ -32,7 +34,7 @@ char PrintGradeLetter(int grade)
 
 int main()
 {
-    int grade = ReadGrade();
+    int grade = ReadNumberInRagne(0, 100);
 
     cout << "You got an " << PrintGradeLetter(grade) << " grade!\n";
     
